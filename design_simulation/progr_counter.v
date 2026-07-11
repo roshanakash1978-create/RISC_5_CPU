60c389d380890f21518c1d96a4d3753edebf23f2
roshanakash1978-create <roshanakash1978@gmail.com>
2026-07-11 20:11:12 +0530

    Update progr_counter.v

--- design_simulation/progr_counter.v
+++ design_simulation/progr_counter.v
@@ -1,15 +1,17 @@
-// pc.v
 `timescale 1ns / 1ps
+
 module pc (
     input  wire        clk,
     input  wire        reset,
-    input  wire [31:0] pc_next, 
+    input  wire [31:0] pc_next,
     output reg  [31:0] pc
 );
+
     always @(posedge clk or posedge reset) begin
         if (reset)
             pc <= 32'd0;
         else
             pc <= pc_next;
     end
+
 endmodule
